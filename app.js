6174fb66968268d55d4892bd383a5b00a25cd924
Saksham1005 <Saksham1005@gmail.com>
2023-08-25 14:14:31 +0530

    Backend Node Server

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const axios = require("axios");
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(express.json());
+app.use(express.urlencoded());
+
+const API_KEY = process.env.API_KEY;
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+
+  next();
+});
+
+app.post("/getWeather", async (req, res) => {
+  try {
+    const { cities } = req.body;
+    const weatherData = {};
+
+    for (const city of cities) {
+      const response = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+      );
+
+      const temperature = response.data.main.temp;
+      weatherData[city] = `${temperature}°C`;
+    }
+
+    res.json({ weather: weatherData });
+  } catch (error) {
+    res.status(500).json({ error: "Error fetching weather data" });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
